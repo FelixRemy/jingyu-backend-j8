28614a0264f824203f70aa2b865766e040fbaf5a
Colin <1983069029@qq.com>
2026-08-21 15:20:17 +0800

    创建项目

---
+++ src/main/java/org/jingyu/jingyubackendj8/JingyuBackendJ8Application.java
@@ -0,0 +1,13 @@
+package org.jingyu.jingyubackendj8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingyuBackendJ8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JingyuBackendJ8Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/jingyu/jingyubackendj8/JingyuBackendJ8ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jingyu.jingyubackendj8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingyuBackendJ8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
